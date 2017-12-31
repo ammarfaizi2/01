de56c58232c9c4860a7f01ce12a55813f502931a
Ammar Faizi <ammarfaizi2@gmail.com>
2017-12-31 22:20:39 +0700

    Init

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "src/A/A.cpp"
+
+int	main() {
+	A instance;
+	instance.test();
+	return 0;
+}
---
+++ src/A/A.cpp
@@ -0,0 +1,14 @@
+#include "A.h"
+
+
+A::A() {
+
+}
+
+A::~A() {
+
+}
+
+void A::test() {
+	std::cout << "Test" << std::endl;
+}
